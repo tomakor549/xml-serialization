7156902629a4fc2651e95b01d883f41ff23afe4c
tomakor549 <venetor7@gmail.com>
2021-08-27 14:48:28 +0200

    create service interface

---
+++ XMLSerializable/ServiceInterface/IServiceRequest.cs
@@ -0,0 +1,14 @@
+﻿using PZePUAP.Headers;
+
+namespace PZePUAP.ServiceInterface
+{
+    /// <summary>
+    /// Marker interface
+    /// </summary>
+    public interface IServiceRequest
+    {
+        string SOAPAction { get; }
+
+        HeaderAttribute[] HeaderAttributes { get; }
+    }
+}
---
+++ XMLSerializable/ServiceInterface/IServiceResponseHandler.cs
@@ -0,0 +1,13 @@
+﻿using PZePUAP.Models.Errors;
+
+namespace PZePUAP.ServiceInterface
+{
+    /// <summary>
+    /// Response handler, creates the response out of the SOAP string
+    /// </summary>
+    public interface IServiceResponseHandler<TResult>
+        where TResult : class, IServiceResponse
+    {
+        TResult FromSOAP(string soapResponse, out FaultModel fault);
+    }
+}
